5ac275b50a9c60bcb9ccc4772a1f23c317d6a36a
David Martinez Marti <deavidsedice@gmail.com>
2018-12-27 21:33:06 +0000

    Enable writes to a certain folder only

--- www/php-apache-1/index.php
+++ www/php-apache-1/index.php
@@ -3,7 +3,14 @@
   <title>Prueba de PHP</title>
  </head>
  <body>
- <?php echo '<p>Hola Mundo</p>'; ?>
- <?php echo time(); ?>
+ <?php
+$filename = './uploads/test.txt';
+echo '<p>Hola Mundo</p>';
+file_put_contents($filename, 'Time: ' . time() . "\n", FILE_APPEND);
+echo '<pre>';
+echo file_get_contents($filename);
+echo '</pre>';
+
+ ?>
  </body>
 </html>
