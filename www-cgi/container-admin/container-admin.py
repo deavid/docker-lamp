e8cb4f2383223a2574bda055c62caa2c88a26532
David Martinez Marti <deavidsedice@gmail.com>
2019-01-05 11:02:06 +0000

    Add form with basic validation

--- www-cgi/container-admin/container-admin.py
+++ www-cgi/container-admin/container-admin.py
@@ -1,6 +1,24 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, url_for, redirect
+from wtforms import Form, StringField, PasswordField, validators
 app = Flask(__name__)
+
+
+class LoginForm(Form):
+    email = StringField(
+        'Email Address',
+        [validators.Length(min=6, max=35)],
+        render_kw={"placeholder": "Your Email"}
+        )
+    token = StringField(
+        'Token',
+        [validators.Length(min=4, max=8)],
+        render_kw={"placeholder": "Your Token from your mobile device"}
+        )
+    password = PasswordField(
+        'Password',
+        [validators.Length(min=6, max=35)],
+        render_kw={"placeholder": "Your Password"}
+        )
 
 
 @app.route("/")
@@ -8,6 +26,12 @@ def hello():
     return render_template('home.html')
 
 
-@app.route("/login/")
+@app.route("/login/", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        email = form.email.data
+        token = form.token.data
+        password = form.password.data
+        return redirect(url_for('login'))
+    return render_template('login.html', form=form)
