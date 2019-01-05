d1799ea8803610ce04d6f51bd44a98ab41836dd3
David Martinez Marti <deavidsedice@gmail.com>
2019-01-05 15:10:03 +0000

    Added login validation against password

--- www-cgi/container-admin/container-admin.py
+++ www-cgi/container-admin/container-admin.py
@@ -1,5 +1,13 @@
+import os
+import os.path
+import sys
+import crypt
+import yaml
+from hmac import compare_digest
 from flask import Flask, render_template, request, url_for, redirect
 from wtforms import Form, StringField, PasswordField, validators
+
+curpath = os.path.abspath(os.path.dirname(sys.argv[0]))
 app = Flask(__name__)
 
 
@@ -33,5 +41,19 @@ def login():
         email = form.email.data
         token = form.token.data
         password = form.password.data
-        return redirect(url_for('login'))
+        with open(os.path.join(curpath, "users.yml"), 'r') as stream:
+            user_db = yaml.load(stream)
+        if email not in user_db:
+            # Stupid password to test against and waste the same CPU to prevent time attacks
+            cryptedpasswd = "$5$6g1mugAfO$Gx6JnJPw6meK5ftNWGGrOpGamXwK/mc1vHlaeTEirf6"
+            compare_digest(crypt.crypt("1231231231232", cryptedpasswd), cryptedpasswd)
+            is_valid = False
+        else:
+            user = user_db[email]
+            cryptedpasswd = user["password"]
+            is_valid = compare_digest(crypt.crypt(password, cryptedpasswd), cryptedpasswd)
+        if not is_valid:
+            form.password.errors.append('The username or password is not valid')
+        if is_valid:
+            return redirect(url_for('login'))
     return render_template('login.html', form=form)
