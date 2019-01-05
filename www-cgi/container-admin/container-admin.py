ae6a75b3edef895b0f585ff5f8224d73c11dc3a4
David Martinez Marti <deavidsedice@gmail.com>
2019-01-05 10:10:14 +0000

    First design for home and login

--- www-cgi/container-admin/container-admin.py
+++ www-cgi/container-admin/container-admin.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('home.html')
+
+
+@app.route("/login/")
+def login():
+    return render_template('login.html')
--- www-cgi/container-admin/flask-uwsgi.sh
+++ www-cgi/container-admin/flask-uwsgi.sh
@@ -3,5 +3,5 @@ test -f config.sh && source config.sh
 export SCRIPT_NAME=container-admin.py
 export FLASK_DEBUG=1
 uwsgi -p4 --lazy-apps --uwsgi-socket 0.0.0.0:3031 -i \
-  --uid 33 --gid 33 \
+  --uid 33 --gid 33 --need-app \
   --manage-script-name --py-autoreload 1 --mount /=container-admin:app
